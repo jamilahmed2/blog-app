bc719edd1062b85b442e86e0d055302f97cbb148
jamilahmed2 <ja0935991@gmail.com>
2023-02-14 23:54:27 +0500

    pages and nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticleList from './pages/ArticleList';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div id="page-body">
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/articles' element={<ArticleList />} />
+            <Route exact path='/articles/:articleId' element={<Article />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Article.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Article = () => {
+  return (
+    <div>Articles</div>
+  )
+}
+
+export default Article
