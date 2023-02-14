6bc69c76c3e940f652cf99a95fe20e5d3d3cce7e
jamilahmed2 <ja0935991@gmail.com>
2023-02-15 02:51:50 +0500

    comments on fake article data array

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,29 +7,45 @@ let articlesInfo = [
     {
         name: "learn-react",
         upvotes: 0,
+        comments: [],
     },
     {
         name: "learn-node",
         upvotes: 0,
+        comments: [],
     },
     {
         name: "learn-mongodb",
         upvotes: 0,
+        comments: [],
     }
 ];
+
 
 app.put('/api/articles/:name/upvotes', (req, res) => {
     const { name } = req.params;
     const article = articlesInfo.find(a => a.name === name);
 
     if (article) {
-        article.upvotes  +=1;
+        article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes} upvotes`);
     } else {
         res.send('The article doen\'t exists');
     }
 })
 
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments)
+    } else {
+        res.send('The article doen\'t exists');
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
