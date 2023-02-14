9b92d355bcab982acb51b88898e6fc24bb85608c
jamilahmed2 <ja0935991@gmail.com>
2023-02-15 02:19:44 +0500

    express url parameters

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
 const app = express();
 const port = 8000
+app.use(express.json());
 
-app.get('/hello',(req,res)=>{
-    res.status(200).send("Hello World!")
+app.post('/hello',(req,res)=>{
+    res.status(200).send(`Hello ${req.body.name}!`)
+})
+
+app.get('/hello/:name',(req,res)=>{
+    const {name}= req.params;
+    res.status(200).send(`Hello ${name}!!`)
 })
 
 app.listen(port,()=>{
