b1449158d2ddb9040129dc0ba446bbcab39c9645
jamilahmed2 <ja0935991@gmail.com>
2023-02-15 02:34:33 +0500

    upvotes endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,34 @@ const app = express();
 const port = 8000
 app.use(express.json());
 
-app.post('/hello',(req,res)=>{
-    res.status(200).send(`Hello ${req.body.name}!`)
+let articlesInfo = [
+    {
+        name: "learn-react",
+        upvotes: 0,
+    },
+    {
+        name: "learn-node",
+        upvotes: 0,
+    },
+    {
+        name: "learn-mongodb",
+        upvotes: 0,
+    }
+];
+
+app.put('/api/articles/:name/upvotes', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.upvotes  +=1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('The article doen\'t exists');
+    }
 })
 
-app.get('/hello/:name',(req,res)=>{
-    const {name}= req.params;
-    res.status(200).send(`Hello ${name}!!`)
-})
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
