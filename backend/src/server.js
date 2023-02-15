f469be7952f0d40bb31a2aca1c9e9484d42fc47b
jamilahmed2 <ja0935991@gmail.com>
2023-02-15 21:27:05 +0500

    rewrite

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { MongoClient } from 'mongodb'
+import {db, connectToDb } from './db.js';
 
 const app = express();
 const port = 8000
@@ -8,11 +8,6 @@ app.use(express.json());
 // fetching data from local database
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params
-
-    const client = new MongoClient('http://127.0.0.1:27107');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
 
     const article = db.collection('articles').findOne({ name })
 
@@ -27,17 +22,12 @@ app.get('/api/articles/:name', async (req, res) => {
 app.put('/api/articles/:name/upvotes', async (req, res) => {
     const { name } = req.params;
 
-    const client = new MongoClient('http://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
     await db.collection('articles').updateOne({ name }, {
         $inc: { upvotes: 1 },
     })
     const article = await db.collection('articles').findOne({ name })
 
     if (article) {
-        article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes} upvotes`);
     } else {
         res.send('The article doen\'t exists');
@@ -45,19 +35,25 @@ app.put('/api/articles/:name/upvotes', async (req, res) => {
 })
 
 // adding comments
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
     const { postedBy, text } = req.body;
-    const article = articlesInfo.find(a => a.name === name);
+
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy, text } },
+    })
+    const article = await db.collection('articles').findOne({ name })
 
     if (article) {
-        article.comments.push({ postedBy, text });
         res.send(article.comments)
     } else {
         res.send('The article doen\'t exists');
     }
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
+connectToDb(() => {
+    console.log("Connected to database");
+    app.listen(port, () => {
+        console.log(`Server is running on ${port}`)
+    })
 })
